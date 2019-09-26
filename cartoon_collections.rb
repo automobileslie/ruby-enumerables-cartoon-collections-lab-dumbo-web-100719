5e8e6248c91d3493182987f5db1ced2499595676
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-26 23:41:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.detect { |snack| snack == cheese_types}
+  snacks.include { |snack| snack == cheese_types}
 end
 
 
